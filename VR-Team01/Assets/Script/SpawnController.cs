627594e6ff752babcd3c19a26dcab950ceaa1118
aerkdude <aerkza_ac129@hotmail.com>
2019-06-19 11:05:09 +0700

    Add SpawnController

---
+++ VR-Team01/Assets/Script/SpawnController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject Paper;
+    //public GameObject[] prefeb;
+    GameObject Temp;
+    private float spawnTime = 5f;
+    public Transform[] spawnPoints;
+
+    void Start()
+    {
+        //int prefeb_num = Random.Range(0, 3);
+        InvokeRepeating("SpawnPaper", spawnTime, spawnTime);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            Destroy(Temp);
+        }
+    }
+
+    void SpawnPaper()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        //Temp = Instantiate(prefeb[prefeb_num], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        Temp = Instantiate(Paper, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
