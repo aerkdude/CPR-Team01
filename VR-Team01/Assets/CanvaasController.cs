328f66b588217457f3dd857e299b7e043bc7280d
pondwatcha <51689439+pondwatcha@users.noreply.github.com>
2019-06-19 11:02:27 +0700

    CanvasQuestion

---
+++ VR-Team01/Assets/CanvaasController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvaasController : MonoBehaviour
+{
+    public GameObject questionCanvas;
+    public bool activeCancas;
+    public float Timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Timer >= 20)
+        {
+           
+            Timer = 0;
+           
+        }
+         if(Timer >= 10 && Timer< 20)
+            {
+                questionCanvas.SetActive(true);
+            }
+            else if (Timer>=20||Timer<10)
+            {
+                questionCanvas.SetActive(false);
+            }
+        CurTimer();
+        
+    }
+   
+    void CurTimer()
+    {
+        Timer+=Time.deltaTime;
+    }
+        
+}
